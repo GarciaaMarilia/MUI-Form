0700682ab78911dc41f8a22d73d9584bd55238f6
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2024-04-06 17:03:02 +0200

    fixing layout

--- mui-form/src/Pages/Form/index.jsx
+++ mui-form/src/Pages/Form/index.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import {
  Button,
  MenuItem,
- TextField,
- Box,
  Grid,
  Paper,
  Typography,
@@ -12,15 +10,19 @@ import {
 import { styled } from "@mui/system";
 import { useForm } from "react-hook-form";
 
+import Box from "@mui/material/Box";
+import AddIcon from "@mui/icons-material/Add";
+import TextField from "@mui/material/TextField";
 import DeleteIcon from "@mui/icons-material/Delete";
-import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 
 const StyledDropZone = styled("div")({
- border: "2px dashed #aaa",
+ border: "2px dashed #a7a7a7",
  borderRadius: "5px",
- padding: "20px",
+ padding: "100px",
+ marginTop: "20px",
  textAlign: "center",
  cursor: "pointer",
+ backgroundColor: "#F8F8FF",
 });
 
 const Formulario = () => {
@@ -72,161 +74,265 @@ const Formulario = () => {
   setSelectedImages(combinedImages);
  };
 
+ const options = ["Opção 1", "Opção 2", "Opção 1"];
+
  return (
-  <Box sx={{ mt: 2, padding: "40px" }}>
-   <Box sx={{ mb: 2 }}>
-    <Typography variant="h5">Informações Gerais</Typography>
-   </Box>
-   <form onSubmit={handleSubmit(onFormSubmit)}>
-    <Grid container spacing={2}>
-     <Grid item xs={6}>
-      <TextField
-       {...register("nome")}
-       label="Nome do Produto"
-       fullWidth
-       margin="normal"
-      />
-     </Grid>
-
-     <Grid item xs={6}>
-      <TextField
-       {...register("tipo")}
-       select
-       label="Tipo"
-       fullWidth
-       margin="normal"
-       defaultValue=""
-      >
-       <MenuItem value="Tipo 1">Tipo 1</MenuItem>
-       <MenuItem value="Tipo 2">Tipo 2</MenuItem>
-       <MenuItem value="Tipo 3">Tipo 3</MenuItem>
-      </TextField>
-     </Grid>
-     <Grid item xs={6}>
-      <TextField
-       {...register("codigo")}
-       label="Código"
-       fullWidth
-       margin="normal"
-      />
-     </Grid>
-
-     <Grid item xs={6}>
-      <TextField
-       {...register("marca")}
-       select
-       label="Marca"
-       fullWidth
-       margin="normal"
-       defaultValue=""
-      >
-       <MenuItem value="Marca 1">Marca 1</MenuItem>
-       <MenuItem value="Marca 2">Marca 2</MenuItem>
-       <MenuItem value="Marca 3">Marca 3</MenuItem>
-      </TextField>
-     </Grid>
-     <Grid item xs={6}>
-      <TextField
-       {...register("fornecedor")}
-       select
-       label="Fornecedor"
-       fullWidth
-       margin="normal"
-       defaultValue=""
-      >
-       <MenuItem value="Fornecedor 1">Fornecedor 1</MenuItem>
-       <MenuItem value="Fornecedor 2">Fornecedor 2</MenuItem>
-       <MenuItem value="Fornecedor 3">Fornecedor 3</MenuItem>
-      </TextField>
-     </Grid>
-
-     <Grid item xs={6}>
-      <TextField
-       {...register("categoria")}
-       select
-       label="Categoria"
-       fullWidth
-       margin="normal"
-       defaultValue=""
-      >
-       <MenuItem value="Categoria 1">Categoria 1</MenuItem>
-       <MenuItem value="Categoria 2">Categoria 2</MenuItem>
-       <MenuItem value="Categoria 3">Categoria 3</MenuItem>
-      </TextField>
-     </Grid>
-     <Grid item xs={12}>
-      <TextField
-       {...register("descricao")}
-       label="Descrição"
-       fullWidth
-       multiline
-       rows={4}
-       margin="normal"
-      />
-     </Grid>
-    </Grid>
-    <Box sx={{ mb: 2, padding: "40px" }}>
-     <Typography variant="h5">Imagens do Produto</Typography>
-    </Box>
-    <Box sx={{ display: "flex", alignItems: "center" }}>
-     <input
-      type="file"
-      accept="image/*"
-      multiple
-      onChange={handleFileChange}
-      style={{ display: "none" }}
-      ref={register("imagens")}
-      id="image-upload"
-     />
-     <label htmlFor="image-upload"></label>
-    </Box>
-
-    <StyledDropZone
-     onDragOver={(e) => e.preventDefault()}
-     onDrop={(e) => handleImageDrop(e)}
-     onClick={() => document.getElementById("image-upload").click()}
+  <form onSubmit={handleSubmit(onFormSubmit)}>
+   <Box sx={{ mt: 2, padding: "40px" }}>
+    <Box
+     sx={{
+      mt: 2,
+      padding: "40px",
+      border: "1px solid #dcdcdc",
+      borderRadius: "8px",
+     }}
     >
-     <IconButton component="span">
-      {" "}
-      <AddPhotoAlternateIcon />
-     </IconButton>
-     <Typography variant="body1">
-      Arraste e solte as imagens aqui ou clique para selecionar
+     <Typography variant="h5" sx={{ textAlign: "left" }}>
+      Informações Gerais
      </Typography>
-    </StyledDropZone>
-
-    <Box sx={{ display: "flex", flexWrap: "wrap", gap: 2, mt: 2 }}>
-     {selectedImages.map((image, index) => (
-      <Paper
-       key={index}
-       sx={{
-        border: mainImageIndex === index ? "2px solid purple" : "none",
-        p: 1,
-        position: "relative",
-       }}
-      >
-       <img
-        src={URL.createObjectURL(image)}
-        alt={`Imagem ${index}`}
-        width="100"
-        style={{ margin: "8px", cursor: "pointer" }}
-        onClick={() => setMainImage(index)}
+     <Grid container spacing={2}>
+      <Grid item xs={6}>
+       <TextField
+        {...register("nome")}
+        label="Nome do produto"
+        fullWidth
+        margin="normal"
        />
-       <Button
-        onClick={() => removeImage(index)}
-        style={{ position: "absolute", top: 0, left: 80 }}
+      </Grid>
+
+      <Grid item xs={6}>
+       <TextField
+        {...register("tipo")}
+        select
+        fullWidth
+        label="Tipo"
+        margin="normal"
+        sx={{ textAlign: "left" }}
        >
-        <DeleteIcon color="purple" />
-       </Button>
-      </Paper>
-     ))}
+        {options.map((option) => {
+         return <MenuItem value={option}>{option}</MenuItem>;
+        })}
+       </TextField>
+      </Grid>
+      <Grid item xs={6}>
+       <TextField
+        {...register("codigo")}
+        label="Código (SKU) do produto"
+        fullWidth
+        margin="normal"
+       />
+      </Grid>
+
+      <Grid item xs={6}>
+       <TextField
+        {...register("marca")}
+        select
+        label="Nome da marca"
+        fullWidth
+        margin="normal"
+        sx={{ textAlign: "left" }}
+       >
+        {options.map((option) => {
+         return <MenuItem value={option}>{option}</MenuItem>;
+        })}
+       </TextField>
+      </Grid>
+      <Grid item xs={6}>
+       <TextField
+        {...register("fornecedor")}
+        select
+        label="Fornecedor"
+        fullWidth
+        margin="normal"
+        sx={{ textAlign: "left" }}
+       >
+        {options.map((option) => {
+         return <MenuItem value={option}>{option}</MenuItem>;
+        })}
+       </TextField>
+      </Grid>
+
+      <Grid item xs={6}>
+       <TextField
+        {...register("categoria")}
+        select
+        label="Categoria"
+        fullWidth
+        margin="normal"
+        sx={{ textAlign: "left" }}
+       >
+        {options.map((option) => {
+         return <MenuItem value={option}>{option}</MenuItem>;
+        })}
+       </TextField>
+      </Grid>
+      <Grid item xs={12}>
+       <TextField
+        {...register("descricao")}
+        label="Descrição"
+        fullWidth
+        multiline
+        rows={4}
+        margin="normal"
+       />
+      </Grid>
+     </Grid>
     </Box>
 
-    <Button type="submit" variant="contained" color="primary" sx={{ mt: 2 }}>
-     Enviar
-    </Button>
-   </form>
-  </Box>
+    {selectedImages.length === 0 ? (
+     <Box
+      sx={{
+       mt: 2,
+       padding: "40px",
+       border: "1px solid #dcdcdc",
+       borderRadius: "8px",
+      }}
+     >
+      <Typography variant="h5" sx={{ textAlign: "left" }}>
+       Imagens do Produto
+      </Typography>
+      <input
+       type="file"
+       accept="image/*"
+       multiple
+       onChange={handleFileChange}
+       style={{ display: "none" }}
+       ref={register("imagens")}
+       id="image-upload"
+      />
+      <label htmlFor="image-upload"></label>
+      <StyledDropZone
+       onDragOver={(e) => e.preventDefault()}
+       onDrop={(e) => handleImageDrop(e)}
+       onClick={() => document.getElementById("image-upload").click()}
+      >
+       <IconButton component="span">
+        {" "}
+        <AddIcon />
+       </IconButton>
+       <Typography variant="h5">
+        Selecione ou arraste aqui as imagens dos produtos
+       </Typography>
+       <Typography variant="body1">
+        Máximo de 15 imagens. Tamanho máximo 4MB.
+        <br />
+        Envie imagens no formato JPG ou PNG.
+       </Typography>
+      </StyledDropZone>
+     </Box>
+    ) : (
+     <Box
+      sx={{
+       display: "flex",
+       flexWrap: "wrap",
+       gap: 2,
+       mt: 2,
+       padding: "40px",
+       border: "1px solid #dcdcdc",
+      }}
+     >
+      <Grid container spacing={10}>
+       <Grid item xs={6}>
+        <Typography variant="h5" sx={{ textAlign: "left" }}>
+         Imagens do Produto
+        </Typography>
+        <Typography variant="body1" sx={{ textAlign: "left", color: "grey" }}>
+         Máximo de 15 imagens. Tamanho máximo 4MB.
+         <br />
+         Envie imagens no formato JPG ou PNG.
+        </Typography>
+       </Grid>
+       <Grid
+        item
+        xs={6}
+        sx={{
+         display: "flex",
+         alignItems: "center",
+         justifyContent: "flex-end",
+        }}
+       >
+        <label
+         htmlFor="image-upload"
+         style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "10px 20px",
+          backgroundColor: "purple",
+          color: "white",
+          borderRadius: "8px",
+          cursor: "pointer",
+         }}
+        >
+         Adicionar imagem
+         <AddIcon sx={{ marginLeft: "5px" }} />
+        </label>
+        <input
+         type="file"
+         accept="image/*"
+         multiple
+         onChange={handleFileChange}
+         style={{ display: "none" }}
+         ref={register("imagens")}
+         id="image-upload"
+        />
+       </Grid>
+      </Grid>
+
+      {selectedImages.map((image, index) => (
+       <Paper
+        key={index}
+        sx={{
+         border: mainImageIndex === index ? "4px solid purple" : "none",
+         p: 1,
+         position: "relative",
+        }}
+       >
+        <img
+         src={URL.createObjectURL(image)}
+         alt={`Imagem ${index}`}
+         width="100"
+         style={{ margin: "8px", cursor: "pointer" }}
+         onClick={() => setMainImage(index)}
+        />
+        {mainImageIndex === index && (
+         <Typography
+          variant="body2"
+          sx={{
+           backgroundColor: "purple",
+           color: "white",
+           position: "absolute",
+           bottom: 0,
+           left: 0,
+           rigth: 0,
+           width: "94%",
+           padding: "5px",
+           textAlign: "center",
+           fontWeight: "bold",
+          }}
+         >
+          Imagem principal
+         </Typography>
+        )}
+        <Button
+         onClick={() => removeImage(index)}
+         style={{ position: "absolute", top: 0, left: 80 }}
+         sx={{
+          backgroundColor: "#D75413",
+          color: "white",
+          borderRadius: "50%",
+          width: "15px",
+          height: "30px",
+         }}
+        >
+         <DeleteIcon />
+        </Button>
+       </Paper>
+      ))}
+     </Box>
+    )}
+   </Box>
+  </form>
  );
 };
 
